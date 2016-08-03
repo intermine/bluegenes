5f12fcd329dad228d290f6adef444928c2f9c68b
Joshua Heimbach <joshkh@gmail.com>
2016-08-03 17:37:38 +0100

    Tidy up code

--- src/cljs/imjs/assets.cljs
+++ src/cljs/imjs/assets.cljs
@@ -2,29 +2,27 @@
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [ajax.core :refer [GET POST]]
             [cljs-http.client :as http]
-            [imjs.utils :as utils]
+            [clojure.xml :as xml]
+            [imjs.utils :as utils :refer [cleanse-url]]
             [cljs.core.async :refer [put! chan <! >! timeout close!]]))
 
 (defn templates
   "Returns the results of a quicksearch"
   [{root :root token :token}]
-  (let [root (utils/cleanse-url root)]
-    (go (:templates (:body (<! (http/get (str root "/templates")
-                                   {:query-params      {:format "json"}
-                                    :with-credentials? false})))))))
+  (go (:templates (:body (<! (http/get (str (cleanse-url root) "/templates")
+                                       {:query-params      {:format "json"}
+                                        :with-credentials? false}))))))
 
 (defn lists
   "Returns the results of a quicksearch"
   [{root :root token :token}]
-  (let [root (utils/cleanse-url root)]
-    (go (:lists (:body (<! (http/get (str root "/lists")
-                                         {:query-params      {:format "json"}
-                                          :with-credentials? false})))))))
+  (go (:lists (:body (<! (http/get (str (cleanse-url root) "/lists")
+                                   {:query-params      {:format "json"}
+                                    :with-credentials? false}))))))
 
 (defn model
   "Returns the results of a quicksearch"
   [{root :root token :token}]
-  (let [root (utils/cleanse-url root)]
-    (go (:lists (:body (<! (http/get (str root "/model")
-                                     {:query-params      {:format "json"}
-                                      :with-credentials? false})))))))
+  (go (:lists (:body (<! (http/get (str (cleanse-url root) "/model")
+                                   {:query-params      {:format "json"}
+                                    :with-credentials? false}))))))
