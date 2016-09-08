caeef5de2fe1092f6670c418d4f4b737e5e17beb
Yo Yehudi <yochannah@gmail.com>
2016-09-08 13:38:34 +0100

    resurrect most of the search from blue genes under /search. needs events adjusted to work without the history flow API.

---
+++ src/cljs/redgenes/components/search/filters.cljs
@@ -0,0 +1,75 @@
+(ns redgenes.components.search.filters
+  (:require [re-frame.core :as re-frame]
+            [json-html.core :as json-html]
+            [reagent.core :as reagent]))
+
+(defn filter-by [criterion state]
+  "sets the active filter. Currently this is a single filter type; we may wish to handle multiples in the future"
+  (swap! state assoc :active-filter criterion)
+)
+
+(defn is-active [name active]
+  "returns whether a given filter is active"
+  (= name active))
+
+(defn remove-filter [filter-name state api search searchterm]
+  "the little x in the corner that allows you to remove filters, and its behaviour"
+  ;;we can probably tidy up the mess of args above.
+  (fn [filter-name state]
+    [:a {
+      :aria-label (str "Remove " filter-name " filter") ;;we need this to stop screen readers from reading the 'x' symbol out loud as though it was meaningful text
+      :on-click (fn [e]
+        (.stopPropagation js/e) ;; if we don't do this the event bubbles to the tr click handler and re-applies the filter. lol.
+        (swap! state dissoc :active-filter)
+        (search searchterm api))}
+      [:span.close "×"]])) ;;that's a cute little &times; to us HTML folk
+
+(defn display-active-filter [active-filter state api search searchterm]
+  "Outputs which filter is active (if any) at the top of the filter section"
+  [:div.active
+   [:h5 "Active filters: "]
+    (if (some? active-filter)
+      [:div.active-filter active-filter [remove-filter active-filter state api search searchterm]]
+      [:div "None"])])
+
+      (defn controls [state]
+        [:form.controls
+         [:div
+         [:label
+          [:input {:type "checkbox" :on-click
+                   (fn [e]
+                     ;;toggle highlight.
+                     (swap! state assoc :highlight-results (.-checked (.-target e))))
+                   }]
+          "Highlight search terms in results (experimental, may be sluggish)"]
+         ]])
+
+(defn facet-display [state api search searchterm]
+  "Visual component which outputs the category filters."
+  (let [facets (:facets @state) active (:active-filter @state)]
+  (if (some? facets)
+  [:div.facets
+    [:h4 "Filter by:"]
+      [display-active-filter active state api search searchterm]
+      [:div
+       ;;TODO: Re-implement this filter when we implement RESTful server-side filters
+      ;  [:h5 "Organisms"]
+      ;  [:table
+      ;   (for [[name value] (:organisms facets)]
+      ;     ^{:key name}
+      ;     [:tr
+      ;      [:td.count value]
+      ;      [:td name]])]
+       [:h5 "Categories"]
+       [:table [:tbody
+      (for [[name value] (:category facets)]
+        ^{:key name}
+        [:tr {
+            :on-click (fn [e] (filter-by name state))
+            :class (if (is-active name active) "active")}
+         [:td.count.result-type {:class (str "type-" name)} value]
+         [:td name (if (is-active name active)
+           [remove-filter name state api search searchterm])]])]
+       ]]
+       [controls state]
+   ])))
--- src/cljs/redgenes/components/search/views.cljs
+++ src/cljs/redgenes/components/search/views.cljs
@@ -1,10 +1,163 @@
 (ns redgenes.components.search.views
   (:require [reagent.core :as reagent]
+            [clojure.string :as str]
+            [redgenes.components.search.resultrow :as resulthandler]
+            [redgenes.components.search.filters :as filters]
             [re-frame.core :as re-frame :refer [subscribe dispatch]]
 ))
 
-(defn main []
-  (fn []
-    [:div
-    [:h1 "SEARCH"  ]
-     ]))
+(def search-results (reagent.core/atom {:results nil}))
+(def max-results 99);;todo - this is only used in a cond right now, won't modify number of results returned. IMJS was being tricky;
+
+(defn sort-by-value [result-map]
+ "Sort map results by their values. Used to order the category maps correctly"
+ (into (sorted-map-by (fn [key1 key2]
+                        (compare [(get result-map key2) key2]
+                                 [(get result-map key1) key1])))
+       result-map))
+
+(defn results-handler [results mine api searchterm]
+   "Store results in local state once the promise comes back."
+   (if (:active-filter @search-results)
+     ;;if we're resturning a filter result, leave the old facets intact.
+     (swap! search-results
+       assoc :results (.-results results))
+     ;;if we're returning a non-filtered result, add new facets to the atom
+     (reset! search-results
+       {
+       :results  (.-results results)
+       :highlight-results (:highlight-results @search-results)
+       :facets {
+         :organisms (sort-by-value (js->clj (aget results "facets" "organism.shortName")))
+         :category (sort-by-value (js->clj (aget results "facets" "Category")))}}))
+   ((:append-state api) {:input searchterm :results @search-results}))
+
+     (defn search
+       "search for the given term via IMJS promise. Filter is optional"
+       [searchterm api & filter]
+         (let [mine (js/imjs.Service. (clj->js {:root "www.flymine.org/query"}))
+               search {:q searchterm :Category filter}
+               id-promise (-> mine (.search (clj->js search)))]
+           (-> id-promise (.then
+               (fn [results]
+                 (results-handler results mine api searchterm))))))
+
+     (defn submit-handler [searchterm api]
+       "Adds search term to the state, and searches for the term"
+       ;(aset js/window "location" "href"
+       ;  (str "/#/timeline/search?" searchterm))
+         (search searchterm api))
+
+     (defn is-active-result? [result]
+       "returns true is the result should be considered 'active' - e.g. if there is no filter at all, or if the result matches the active filter type."
+         (or
+           (= (:active-filter @search-results) (.-type result))
+           (nil? (:active-filter @search-results))))
+
+     (defn count-total-results [state]
+       "returns total number of results by summing the number of results per category. This includes any results on the server beyond the number that were returned"
+       (reduce + (vals (:category (:facets state))))
+       )
+
+     (defn count-current-results []
+       "returns number of results currently shown, taking into account result limits nd filters"
+       (count
+         (remove
+           (fn [result]
+             (not (is-active-result? result))) (:results @search-results))))
+
+     (defn results-count []
+       "Visual component: outputs the number of results shown."
+         [:small " Displaying " (count-current-results) " of " (count-total-results @search-results) " results"])
+
+     (defn load-more-results [api search-term]
+       (.log js/console (clj->js @search-term))
+       (search @search-term api (:active-filter @search-results))
+       )
+
+     (defn results-display [api search-term]
+       "Iterate through results and output one row per result using result-row to format. Filtered results aren't output. "
+       [:div.results
+         [:h4 "Results for '" @(re-frame/subscribe [:search-term]) "'"  [results-count]]
+         [:form
+           (doall (let [state search-results
+             ;;active-results might seem redundant, but it outputs the results we have client side
+             ;;while the remote results are loading. Good for slow connections.
+             active-results (filter (fn [result] (is-active-result? result)) (:results @state))
+             filtered-result-count (get (:category (:facets @state)) (:active-filter @state))]
+               ;;load more results if there are less than our preferred number, but more than
+               ;;the original search returned
+               (cond (and  (< (count-current-results) filtered-result-count)
+                           (<= (count-current-results) max-results))
+                 (load-more-results api search-term))
+               ;;output em!
+               (for [result active-results]
+                 ^{:key (.-id result)}
+                 [resulthandler/result-row {:result result :state state :api api :search-term @search-term}])))]
+        ])
+
+     (defn check-for-query-string-in-url []
+       "Splits out the search term from the URL, allowing repeatable external linking to searches"
+       (let [url (aget js/window "location" "href")
+             last-section (str/split url #"/search\?")]
+         (if (> (count last-section) 1) ;; if there's a query param, eg "someurl.com/#/timeline/search?fkh"
+           (re-frame/dispatch [:set-search-term (last last-section)])
+           nil)))
+
+     (defn search-form [search-term api]
+       "Visual form component which handles submit and change"
+       [:div.search-fullscreen
+         [:form.searchform {:on-submit (fn [e]
+           (.preventDefault js/e)
+           (let [input (.querySelector (.-target e) "input")
+                 val (.-value input)]
+             (re-frame/dispatch [:set-search-term val])
+             (submit-handler val api)
+             (set! (.-value input) ""))
+           )}
+
+             [:input {
+               :type "text"
+               :placeholder "Search for a gene, protein, disease, etc..."}]
+         [:button "Submit"]]
+        [:div.response
+           [filters/facet-display search-results api search @search-term]
+           [results-display api search-term]]])
+
+     (defn ^:export main []
+       (let [global-search-term (re-frame/subscribe [:search-term])]
+       (reagent/create-class
+         {:reagent-render
+           (fn render [{:keys [state upstream-data api]}]
+             (.log js/console "%cRender. Search term is '%s'" "color:hotpink;font-weight:bold;" @global-search-term)
+             [search-form global-search-term api]
+             )
+           :component-will-mount (fn [this]
+             (let [passed-in-state (:state (reagent/props this))
+                   query-string (check-for-query-string-in-url)
+                   api (:api (reagent/props this))]
+               ;(reset! local-search-term (:input passed-in-state))
+               (swap! search-results assoc :query-string query-string)
+               ;populate the form from the url if there's a query param
+               (cond (some? global-search-term)
+                 (do
+                   (.log js/console "%cMounted. Search term is '%s'" "color:cornflowerblue;font-weight:bold;" @global-search-term)              ;(reset! local-search-term @global-search-term)
+                   (submit-handler @global-search-term api)
+                   ))))
+           :component-did-update (fn [this]
+                     (.log js/console "%cUpdated. Search term is '%s'" "color:lightslategrey;font-weight:bold;" @global-search-term))
+           :component-will-update (fn [this]
+             (let [api (:api (reagent/props this))]
+             (.log js/console "%cYay, I'm gonna update. Search term is '%s'" "color:seagreen;font-weight:bold;" @global-search-term)
+             (.log js/console "%cProps" "color:seagreen;font-weight:bold;" (clj->js (reagent/props this)))
+               ;(re-frame/dispatch [:set-search-term @local-search-term])
+               (submit-handler @global-search-term api)))
+           :should-component-update (fn [this]
+               ;"Only update if there's a new search term. Otherwise we end up in a loop of updating forever and ever and ever and.... voom. DOS."
+               ;(.log js/console "%cShould I update? old search term is '%s', new is '%s'" "color:lightseagreen;font-weight:bold;" @local-search-term @global-search-term)
+               ;(not= @global-search-term @local-search-term)
+             )
+           :component-will-receive-props (fn [this]
+             (.log js/console "%cwill get props. Search term is '%s'" "color:yellowgreen;font-weight:bold;" @global-search-term))
+
+     })))
