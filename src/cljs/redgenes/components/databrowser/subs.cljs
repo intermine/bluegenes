92621699b928bf971e0b9c646035c1df653d3916
Yochannah Yehudi <yochannah@gmail.com>
2016-09-13 15:20:43 +0100

    we have bubbles. not positioned or sized correctly, but we have bubbles.

--- src/cljs/redgenes/components/databrowser/subs.cljs
+++ src/cljs/redgenes/components/databrowser/subs.cljs
@@ -2,7 +2,18 @@
   (:require-macros [reagent.ratom :refer [reaction]])
   (:require [re-frame.core :as re-frame :refer [reg-sub]]))
 
-(reg-sub
-  :databrowser/whitelist
-  (fn [db _]
-    (:databrowser/whitelist db)))
+  (reg-sub
+    :databrowser/whitelist
+    (fn [db _]
+      (:databrowser/whitelist db)))
+
+  (reg-sub
+    :databrowser/whitelisted-model
+    (fn [db _]
+      ;;We'll use these filtered values repeatedly so let's do it in one place
+      (let [model (:model (:assets db))
+            whitelist (:databrowser/whitelist db)]
+        (keep (fn [vals]
+          (cond (contains? whitelist (first vals)) vals)
+        ) model)
+  )))
--- src/cljs/redgenes/components/databrowser/views.cljs
+++ src/cljs/redgenes/components/databrowser/views.cljs
@@ -6,25 +6,35 @@
 (defn visual-filter
   "Visual and interactive UI component allowing the userto view selected model properties in a textual form." []
   [:div.filter [:h4 "Filter: "]
-    (let [model (subscribe [:model])
-          whitelist (subscribe [:databrowser/whitelist])]
+    (let [model (subscribe [:databrowser/whitelisted-model])]
       (into [:div.filter-by]
         (map (fn [[id vals]]
-               (cond (contains? @whitelist id)
-             [:p id])
-         ) @model)
-
-
-            )
-;      (.log js/console "%c@whitelist" "color:hotpink;font-weight:bold;"  @whitelist )
+             [:p (:name vals)]
+        ) @model))
   )])
+
+(defn bubbles
+  "Visual and interactive UI component allowing user to view model properties more visually than the text filters. " []
+  [:div.bubbles [:h2 "Explore: "]
+    (let [model (subscribe [:databrowser/whitelisted-model])
+          r 30]
+          (into [:svg {:version "1.1", :viewbox "0 0 500 500"}]
+            (map-indexed (fn [index [id vals]]
+              (let [r-positioned (* r (inc index))]
+              [:g [:circle {:r r, :cy r-positioned, :cx r-positioned :fill "rgba(80, 160, 240, 0.3)"}]
+              [:text {:x r-positioned :y r-positioned :text-anchor "middle"} (:name vals)]]
+            )) @model))
+
+
+
+      )
+
+])
 
 (defn main []
   (fn []
     [:div.databrowser
       [visual-filter]
-     [:div.bubbles [:h2 "Explore: "]
-        [:div "Bubbles here."]
-      ]
+      [bubbles]
      [:div.preview [:h3 "Preview your data"]]
      ]))
