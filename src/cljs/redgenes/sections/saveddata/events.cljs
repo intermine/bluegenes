caa48b464c50ad0bdbb28a78a6634793717b3ea5
Yochannah Yehudi <yochannah@gmail.com>
2016-09-13 10:54:28 +0100

    removed duplicated subs that were causing warnings in the console

--- src/cljs/redgenes/sections/saveddata/events.cljs
+++ src/cljs/redgenes/sections/saveddata/events.cljs
@@ -2,8 +2,3 @@
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [re-frame.core :refer [reg-event-db reg-event-fx reg-fx dispatch]]
             [cljs.core.async :refer [put! chan <! >! timeout close!]]))
-
-(reg-event-db
-  :results/set-query
-  (fn [db [_ query]]
-    (assoc-in db [:results :query] query)))
