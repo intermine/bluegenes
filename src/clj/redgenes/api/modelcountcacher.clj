ce4ed91f79f983d76842da879946d7f13cd90922
Yo Yehudi <yochannah@gmail.com>
2016-09-21 17:04:31 +0100

    asynchronously stores all the top level model count values in a redis on locahost:6379, assuming such a regist exists, when localhost:3000/model/count/cache/ recieves a GET

--- src/clj/redgenes/api/modelcountcacher.clj
+++ src/clj/redgenes/api/modelcountcacher.clj
@@ -1,36 +1,38 @@
 (ns redgenes.api.modelcountcacher
   (:require [clj-http.client :as client]
             [org.httpkit.client :as http]
-    [redgenes.whitelist :as config]
+            [redgenes.whitelist :as config]
+            [redgenes.redis :refer [wcar*]]
+            [taoensso.carmine :as car :refer (wcar)]
 ))
 
+(defn store-response [item mine thecount]
+  (wcar* (car/hset (str "modelcount-" mine) item thecount))
+  (println "response 3" item mine thecount)
+)
+
 (defn count-query [path]
-    ;(str "%3Cquery%20model%3D%22genomic%22%20view%3D%22" path ".id%22%20%3E%3C%2Fquery%3E")
     (str "<query model=\"genomic\" view=\"" path ".id\" ></query>")
   )
 
-(defn get-count [item mine]
-  (println item mine (count-query (name item)))
-   (let [count (client/post (str mine "/query/results")
-     {:form-params
-       {:format "count"
-        :query (count-query (name item))}
-
-      ;:client-params {"format" "json"
-      ;                "query" (count-query (name item))}
-      })]
-     (println (:body count))
-   )
-  )
+(defn get-count
+"asynchronously loads the count for a given path"
+  [item mine]
+  (http/post (str mine "/query/results")
+    {:form-params
+     {:format "count"
+      :query (count-query (name item))}}
+    (fn [{:keys [status headers body error]}] ;; asynchronous response handling
+      (if error
+        (println "Failed, exception is " error) ;;Oh noes :(
+        (store-response item mine body) ;;success - store the stuff!
+))))
 
 (defn load-model []
   (let [mine "http://beta.flymine.org/query/service"
         model (client/get (str mine "/model?format=json") {:keywordize-keys? true :as :json})
         whitelisted-model (select-keys (:classes (:model (:body model))) config/whitelist)
-        thingy (map (fn [x y] (println x y)) (keys whitelisted-model))]
-    (println "Jordan's alive! whitelisted-model" (keys whitelisted-model) )
-    (doall (map #(get-count % mine) (keys whitelisted-model)))
-;    (doall (map println (keys whitelisted-model)))
-;    (doall (map (fn [x y] (println x y)) (keys whitelisted-model)))
+        promises (doall (map #(get-count % mine) (keys whitelisted-model)))
+        ]
     model
-  ))
+))
