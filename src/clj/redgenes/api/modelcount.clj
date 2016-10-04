3a6615c8252285c56366586b6b9a490e2fb13020
Yo Yehudi <yochannah@gmail.com>
2016-10-04 12:12:21 +0100

    added conditional to allow you to query fo all top level path counts.

--- src/clj/redgenes/api/modelcount.clj
+++ src/clj/redgenes/api/modelcount.clj
@@ -2,6 +2,7 @@
   (:require [ring.util.response :refer [response]]
             [redgenes.redis :refer [wcar*]]
             [redgenes.mines :as mines]
+            [redgenes.whitelist :as config]
             [clojure.string :refer [split trim]]
             [redgenes.api.modelcountcacher :as cacher]
             [ring.middleware.params :refer [wrap-params]]
@@ -15,9 +16,16 @@
   "
   [paths mine]
   ;;TODO / note 3: I couldn't for the life of me get parameters to parse an array of values into a vector. Who knows why. I'd really rather *not* be splitting strings. I tried various ring middlewares and the only one that works for json parameters seemed to be ring.middleware.params/wrap-params. See the figwheel.clj for deets.
+
   ;;This next line doesn't have to be hgetall. We could individually select keys via hget or hmget if it's faster and matters.
-  (select-keys (wcar* (car/parse-map (car/hgetall (str "modelcount-" mine)))) (map #(trim %) (split paths #",")))
-  )
+  (let [responts (wcar* (car/parse-map (car/hgetall (str "modelcount-" mine))))]
+    (if (= paths "top")
+      ;;give all the paths at the top level, which incidentally appear on the whitelist
+      (select-keys responts (map #(name %) config/whitelist))
+      ;;give only selected paths
+      (select-keys responts (map #(trim %) (split paths #","))))
+
+  ))
 
   ;;;;;;;;;;;;; some longer term todo notes:
   ;;TODO 2: when this gets more advanced ensure it is self documenting in some way. swagger, openapi,
