0635141f1b49d6f4aea8eb5e7f846ad5738574d9
Yo Yehudi <yochannah@gmail.com>
2016-10-10 13:14:17 +0100

    add feature to lighttable. pass {:title true} as second light table arg to automatically add a title in the form of the class's display name. yay.

--- src/cljs/imcljs/names.cljs
+++ src/cljs/imcljs/names.cljs
@@ -4,9 +4,9 @@
 (defn find-name
   "Given a collection look up the *display name* for the object" ;; I expect this to grow complexer.
   [thing-to-find-name-for]
-  (let [the-name (:referencedType thing-to-find-name-for)
+  (let [reference-name (:referencedType thing-to-find-name-for)
+        the-name (if (some? reference-name) reference-name thing-to-find-name-for)
         model (subscribe [:model])]
-;    (.log js/console "%cmodel" "color:hotpink;font-weight:bold;" (clj->js @model))
     (:displayName ((keyword the-name) @model))
 ))
 
--- src/cljs/redgenes/components/lighttable.cljs
+++ src/cljs/redgenes/components/lighttable.cljs
@@ -1,6 +1,7 @@
 (ns redgenes.components.lighttable
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [reagent.core :as reagent]
+            [imcljs.names :refer [find-name]]
             [re-frame.core :as re-frame :refer [subscribe dispatch]]
             [dommy.core :as dommy :refer-macros [sel sel1]]
             [cljs.core.async :refer [put! chan <! >! timeout close!]]
@@ -16,8 +17,11 @@
   (map (fn [column] (apply = (map (fn [row] (nth row column)) table))) (range (count (first table)))))
 
 
-(defn table []
-  (fn [results]
+(defn table
+  "a basic results table without imtables complexity. optional second arg options allows you to specify whether or not to show a title for the table, as {:title true}. "
+  []
+  (fn
+    ([results]
     (let [skip-columns nil #_(homogeneous-columns (:results results))]
       [:table.table.small
        [:thead
@@ -32,21 +36,32 @@
                                            [:td.skipped "..."]
                                            [:td (if (< 50 (count (str value)))
                                                   (str (apply str (take 50 (str value))) "...")
-                                                  (str value))])) row))) (:results results)))])))
+                                                  (str value))])) row))) (:results results)))]))
+
+  ([results options]
+   (if (:title options) ;; we could tweak this further to make a nice passed-in title, too
+    [:div
+      [:h4 (find-name (:class results))]
+      [table results]]
+   [table results])
+  )))
+
+;;;;;
+;(find-name (:class query))
+;;;
 
 (defn shell []
-  (fn [state]
+  (fn [state q options]
     (if (empty? (:results @state))
       [:div.small.no-results (str (:class @state) " - No Results") ]
-      [:div.lt [table @state]])))
+      [:div.lt [table @state options]])))
 
 (defn handler [state e]
   (let [props (reagent/props e)
         node  (sel1 (reagent/dom-node e) :.im-target)]
-    (go (let [new-results (<! (search/raw-query-rows {:root @(subscribe [:mine-url])}
-                                                 (:query props)
-                                                 {:size   5
-                                                  :format "json"}))]
+    (go (let [new-results (<! (search/raw-query-rows {:root @(subscribe [:mine-url])} (:query props)
+                  {:size   5
+                   :format "json"}))]
       ;; assoc the original class so we can say what has no results
       (reset! state (assoc new-results :class (:class props)))
       ))
@@ -57,4 +72,4 @@
     (reagent/create-class
       {:component-did-mount  (partial handler state)
        :component-did-update (partial handler state)
-       :reagent-render       (fn [] [shell state])})))
+       :reagent-render       (fn [q options] [shell state q options])})))
