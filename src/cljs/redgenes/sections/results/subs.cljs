eac257308e11cd6524c2371f87b781f0dc41440b
Joshua Heimbach <joshkh@gmail.com>
2016-10-12 18:07:16 +0100

    Add enrichment statistics to /results route

--- src/cljs/redgenes/sections/results/subs.cljs
+++ src/cljs/redgenes/sections/results/subs.cljs
@@ -2,7 +2,26 @@
   (:require-macros [reagent.ratom :refer [reaction]])
   (:require [re-frame.core :refer [reg-sub]]))
 
+
+
+
+
 (reg-sub
   :results/query
   (fn [db]
     (get-in db [:results :query])))
+
+(reg-sub
+  :results/query-parts
+  (fn [db]
+    (get-in db [:results :query-parts])))
+
+(reg-sub
+  :results/enrichment-results
+  (fn [db _]
+    (get-in db [:results :enrichment-results])))
+
+(reg-sub
+  :results/text-filter
+  (fn [db]
+    (get-in db [:results :text-filter])))
