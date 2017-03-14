bd88c29833a07aaf546636d450a81cac1e869ca3
Joshua Heimbach <joshkh@gmail.com>
2017-03-14 11:10:30 +0000

    Queries generated for enrichment should include all keys, not just :select and :where

--- src/cljs/imcljsold/filters.cljs
+++ src/cljs/imcljsold/filters.cljs
@@ -124,14 +124,14 @@
 (defn get-parts
   "Get all of the different parts of an intermine query and group them by type"
   [model query]
+  (.log js/console "query" query)
   (group-by :type
             (distinct
               (map (fn [path]
                      (assoc {}
                        :type (im-type model path)
                        :path (str (trim-path-to-class model path) ".id")
-                       :query {:select [(str (trim-path-to-class model path) ".id")]
-                               :where (:where query)}))
+                       :query (merge query {:select [(str (trim-path-to-class model path) ".id")]}) ))
                    (:select query)))))
 
 
