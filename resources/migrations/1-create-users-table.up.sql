c41650564a2eb2181de3741d9dd5c9587a6baca8
Joshua Heimbach <joshkh@gmail.com>
2017-07-27 12:02:03 +0100

    Rename user_id to id

--- resources/migrations/1-create-users-table.up.sql
+++ resources/migrations/1-create-users-table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-    user_id UUID DEFAULT uuid_generate_v4() UNIQUE NOT NULL PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() UNIQUE NOT NULL PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     password TEXT,
     role TEXT
