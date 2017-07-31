ce1c2504b13538b98cc26799c3dffa692de03cd1
Yo Yehudi <yochannah@gmail.com>
2017-07-31 15:48:51 +0100

    replace two more icons, beautify icon file,

--- src/cljs/bluegenes/components/enrichment/views.cljs
+++ src/cljs/bluegenes/components/enrichment/views.cljs
@@ -87,9 +87,13 @@
            [:span (if results (str " (" (count results) ")"))]
            (if (< 0 (count results))
              [:span
+              ;;TODO: replace the i.fa below with the svg icon when enrichment is fixed.
+              ;;[:svg.icon.icon-circle-left [:use {:xlinkHref "#icon-circle-left"}]]
               [:i.fa.fa-caret-left
                {:on-click (fn [] (swap! page-state update :page dec))
                 :title "View previous 5 enrichment results"}]
+                ;;TODO: replace the i.fa below with the svg icon when enrichment is fixed.
+                ;;[:svg.icon.icon-circle-right [:use {:xlinkHref "#icon-circle-right"}]]
               [:i.fa.fa-caret-right
                {:on-click (fn [] (swap! page-state update :page inc))
                 :title "View next 5 enrichment results"}]])]
