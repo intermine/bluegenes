a2028894e374322b569068365a64d01de48c57f8
Joshua Heimbach <joshkh@gmail.com>
2017-08-29 10:54:32 +0100

    Do not exit if DB connection cannot be established

--- src/clj/bluegenes/server.clj
+++ src/clj/bluegenes/server.clj
@@ -4,7 +4,7 @@
             [ring.adapter.jetty :refer [run-jetty]]
             [bluegenes.mounts :as mounts]
             [mount.core :as mount]
-            [taoensso.timbre :as timbre :refer [infof]]
+            [taoensso.timbre :as timbre :refer [infof errorf]]
             [bluegenes.migrations :as migrations])
   (:gen-class))
 
@@ -19,7 +19,10 @@
 (defn -main [& args]
   (let [port (->int (or (:server-port env) 5000))]
     (timbre/set-level! :info)
-    (mount/start)
-    (migrations/migrate)
+    (try
+      (do
+        (mount/start)
+        (migrations/migrate))
+      (catch Exception e (errorf "Unable to connect to database: %s" (.getMessage e))))
     (run-jetty handler {:port port :join? false})
     (infof "Bluegenes server started on port: %s" port)))
