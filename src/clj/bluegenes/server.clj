4cc63ca70986571f4f3211cb64af12deee885ff7
Joshua Heimbach <joshkh@gmail.com>
2017-08-29 10:54:32 +0100

    Merge server startup updates

--- src/clj/bluegenes/server.clj
+++ src/clj/bluegenes/server.clj
@@ -1,10 +1,28 @@
 (ns bluegenes.server
   (:require [bluegenes.handler :refer [handler]]
             [config.core :refer [env]]
-            [ring.adapter.jetty :refer [run-jetty]])
-  ;(:gen-class)
-  )
+            [ring.adapter.jetty :refer [run-jetty]]
+            [bluegenes.mounts :as mounts]
+            [mount.core :as mount]
+            [taoensso.timbre :as timbre :refer [infof errorf]]
+            [bluegenes.migrations :as migrations])
+  (:gen-class))
 
- (defn -main [& args]
-   (let [port (Integer/parseInt (or (env :port) "3000"))]
-     (run-jetty handler {:port port :join? false})))
+(defn ->int
+  "Force a value to a number (environment variables are read as strings)"
+  [n]
+  (cond
+    (string? n) (read-string n)
+    (int? n) n
+    :else n))
+
+(defn -main [& args]
+  (let [port (->int (or (:server-port env) 5000))]
+    (timbre/set-level! :info)
+    (try
+      (do
+        (mount/start)
+        (migrations/migrate))
+      (catch Exception e (errorf "Unable to connect to database: %s" (.getMessage e))))
+    (run-jetty handler {:port port :join? false})
+    (infof "Bluegenes server started on port: %s" port)))
