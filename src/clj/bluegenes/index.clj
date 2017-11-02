6f289e403911a81d3339a6afdaaeec8493e8e0cc
Joshua Heimbach <joshkh@gmail.com>
2017-11-02 11:45:29 +0000

    Include Google API library for OAuth2 authentication

--- src/clj/bluegenes/index.clj
+++ src/clj/bluegenes/index.clj
@@ -16,26 +16,27 @@
 (defn head []
   [:head
    loader-style
-    [:title "InterMine 2.0 bluegenes"]
-    (include-css
-      "https://cdnjs.cloudflare.com/ajax/libs/gridlex/2.2.0/gridlex.min.css")
-    (include-css
-      "http://cdn.intermine.org/js/intermine/im-tables/2.0.0/main.sandboxed.css")
-    (include-css "css/site.css")
-    [:meta {:charset "utf-8"}]
-    [:meta {:content "width=device-width, initial-scale=1", :name "viewport"}]
-    [:link {:rel "shortcut icon" :href "https://cdn.rawgit.com/intermine/design-materials/f5f00be4/logos/intermine/fav32x32.png" :type "image/png"}]
-    [:script {:src
-      "http://cdn.intermine.org/js/intermine/imjs/3.15.0/im.min.js"}]
-    [:script
-     {:crossorigin "anonymous",
-      :integrity "sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=",
-      :src "https://code.jquery.com/jquery-3.1.0.min.js"}]
-    [:script {:src "vendor/bootstrap/dist/js/bootstrap.js"}]
-    [:script {:src "vendor/bootstrap/js/tooltip.js"}]
-    [:script {:src "vendor/bootstrap/js/popover.js"}]
-  ]
-)
+   [:title "InterMine 2.0 bluegenes"]
+   (include-css
+     "https://cdnjs.cloudflare.com/ajax/libs/gridlex/2.2.0/gridlex.min.css")
+   (include-css
+     "http://cdn.intermine.org/js/intermine/im-tables/2.0.0/main.sandboxed.css")
+   (include-css "css/site.css")
+   [:meta {:charset "utf-8"}]
+   [:meta {:content "width=device-width, initial-scale=1", :name "viewport"}]
+   [:link {:rel "shortcut icon" :href "https://cdn.rawgit.com/intermine/design-materials/f5f00be4/logos/intermine/fav32x32.png" :type "image/png"}]
+   [:script {:src
+             "http://cdn.intermine.org/js/intermine/imjs/3.15.0/im.min.js"}]
+   [:script
+    {:crossorigin "anonymous",
+     :integrity "sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=",
+     :src "https://code.jquery.com/jquery-3.1.0.min.js"}]
+   [:script {:src "vendor/bootstrap/dist/js/bootstrap.js"}]
+   [:script {:src "vendor/bootstrap/js/tooltip.js"}]
+   [:script {:src "vendor/bootstrap/js/popover.js"}]
+   [:script {:src "https://apis.google.com/js/api.js"}]
+   ]
+  )
 
 (defn loader []
   [:div#wrappy
@@ -53,11 +54,11 @@
 (defn index [identity]
   (let [json-identity (json/generate-string identity)]
     (html5
-     (head)
-     [:body
-      (loader)
-      [:div#app]
-      [:script {:src (str "js/compiled/app.js?v=" (version))}]
-      [:script (str "bluegenes.core.init(" json-identity ");")]
-      ]
-     )))
+      (head)
+      [:body
+       (loader)
+       [:div#app]
+       [:script {:src (str "js/compiled/app.js?v=" (version))}]
+       [:script (str "bluegenes.core.init(" json-identity ");")]
+       ]
+      )))
