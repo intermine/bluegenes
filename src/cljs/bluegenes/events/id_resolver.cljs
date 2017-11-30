a8a71bbc2756d177bd400ff83a17108a712c29fa
Joshua Heimbach <joshkh@gmail.com>
2017-11-30 11:37:51 +0000

    Add events for staging files for upload

--- src/cljs/bluegenes/events/id_resolver.cljs
+++ src/cljs/bluegenes/events/id_resolver.cljs
@@ -1,3 +1,10 @@
 (ns bluegenes.events.id-resolver
   (:require [re-frame.core :refer [reg-event-db reg-event-fx]]))
 
+(reg-event-db ::stage-file
+              (fn [db [_ js-File]]
+                (update-in db [:idresolver :files] conj js-File)))
+
+(reg-event-db ::unstage-file
+              (fn [db [_ js-File]]
+                (update-in db [:idresolver :files] #(remove (partial = js-File) %))))
