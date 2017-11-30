9f969c09a1a08d356e2e17d6097b8912ae642a43
Joshua Heimbach <joshkh@gmail.com>
2017-11-30 13:02:14 +0000

    Parse identifiers from file-uploads and the request body

--- src/clj/bluegenes/ws/ids.clj
+++ src/clj/bluegenes/ws/ids.clj
@@ -14,10 +14,21 @@
          (take-while some?)
          (map (partial (comp last (partial take-while some?)))))))
 
+(defn parse-file [[file-name {:keys [filename content-type tempfile size]}]]
+  (parse-identifiers (slurp tempfile)))
+
 (defroutes routes
-           ; Expects a POST raw body of type text/plain identifiers
-           (wrap-multipart-params (POST "/parse" {body :body :as req}
-                                    (response/ok (let [parsed (parse-identifiers (slurp body))]
-                                                   {:identifiers parsed
-                                                    :total (count parsed)})))))
+           ; Expects one or more of the following:
+           ; * Raw body of type text/plain identifiers
+           ; * Mutlipart-params with files of identifiers
+           (wrap-multipart-params (POST "/parse" {:keys [body multipart-params] :as req}
+                                    (let [
+                                          ; Parse identifiers from file upload
+                                          from-upload (mapcat parse-file multipart-params)
+                                          ; Parsed identifiers from the raw body
+                                          from-body   (parse-identifiers (slurp body))]
+                                      ; Concat the results together and return unique values
+                                      (response/ok (let [total (distinct (concat from-upload from-body))]
+                                                     {:identifiers total
+                                                      :total (count total)}))))))
 
