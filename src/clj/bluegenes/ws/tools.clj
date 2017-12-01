f954f4692383a313fb5a92db27bde619e8c27e8a
Yo Yehudi <yochannah@gmail.com>
2017-12-01 12:25:31 +0000

    update tool api method to return config for all tools

--- src/clj/bluegenes/ws/tools.clj
+++ src/clj/bluegenes/ws/tools.clj
@@ -1,21 +1,27 @@
 (ns bluegenes.ws.tools
   (:require [compojure.core :refer [GET POST defroutes]]
             [compojure.route :as route]
-            [clojure.string :refer [blank?]]
+            [clojure.string :refer [blank? join]]
             [cheshire.core :as cheshire]
             [config.core :refer [env]]
             [clojure.pprint :refer [pprint]]
             [ring.util.http-response :as response]
             [clojure.java.io :as io]))
 
+(defn get-tool-config [tool path]
+  (let [name (.getName (io/file tool))]
+    {:name name
+     :config (cheshire/parse-string (slurp (join "/" [path name "config.json"])))
+   }))
+
 (defn tools [session]
   (let [file "resources/public/tools"
         res {:tools
              (reduce
-                   (fn [coll newitem]
+                   (fn [tool-list newitem]
                      (if (.isDirectory (io/file newitem))
-                       (conj coll (.getName (io/file newitem)))
-                       coll))
+                       (conj tool-list (get-tool-config newitem file))
+                       tool-list))
                    #{} (.listFiles (io/file file)))}]
     (response/ok res)))
 
