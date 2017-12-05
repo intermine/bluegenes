b93947848e70dbecb2df080877067b2898d1179f
Yo Yehudi <yochannah@gmail.com>
2017-12-05 10:46:07 +0000

    refactor tool api page

--- src/cljs/bluegenes/sections/developer/tools.cljs
+++ src/cljs/bluegenes/sections/developer/tools.cljs
@@ -4,21 +4,36 @@
             [bluegenes.subs.developer :as subs]
             [accountant.core :refer [navigate!]]))
 
-(defn tool-store []
+(def page-types {:report-page "Report page"
+                 :list-results-page "List results page"})
+(def tool-types {"id" :report-page
+                 "ids" :list-results-page
+                 "list" :list-results-page
+                 "lists" :list-results-page
+                 "records" :list-results-page
+                 "rows" :list-results-page
+                 "query" :list-results-page})
 
+(defn get-tool-types [tool]
+  (into [:span]
+        (map
+          (fn [tool-type]
+            (let [the-type (get tool-types tool-type)]
+            (get page-types the-type)))
+              (get-in tool [:config :accepts]))))
+
+(defn tool-list []
+  (let [tools (subscribe [::subs/tools])]
+    (.log js/console "%c@tools" "color:mediumorchid;font-weight:bold;" (clj->js @tools))
+    (into [:div] (map (fn [tool]
+                        [:div.tool
+                         [:h4 (get-in tool [:package :name])]
+                         [:h5 "Tool Type:"] [get-tool-types tool]
+                         [:h5 "Suitable for pages with these classes:"] [:span (clojure.string/join ", " (get-in tool [:config :classes]))]])  (:tools @tools)))))
+
+(defn tool-store []
   [:div
    [:h1 "Tool Store"]
    [:div.panel.container
-    (let [tools (subscribe [::subs/tools])]
-       (.log js/console "%c@tools" "color:mediumorchid;font-weight:bold;" (clj->js @tools))
-       (into [:div] (map (fn [tool]
-                           [:div.tool
-                            [:h4 (get-in tool [:package :name])]
-                            [:dl
-                            [:dt "Accepts" ] [:dd (clojure.string/join ", " (get-in tool [:config :accepts]))]
-                            [:dt "Classes" ] [:dd (clojure.string/join ", " (get-in tool [:config :classes]))]
-                             ]
-                            ])  (:tools @tools)))
-
-      )
-    ]])
+    [:div "Showing all tools that are currently installed for Report Pages"]
+    [tool-list]]])
