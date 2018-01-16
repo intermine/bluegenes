45cea8f047ab5e9b5297dcc8cd9da9e5c7bd3a9d
Joshua Heimbach <joshkh@gmail.com>
2018-01-16 11:19:54 +0000

    Comments

--- src/clj/bluegenes/migrations.clj
+++ src/clj/bluegenes/migrations.clj
@@ -16,10 +16,14 @@
       (assoc :classname "org.postgresql.ds.PGSimpleDataSource"
              :ssl-mode "disable")))
 
-(defn migrate []
+(defn migrate
+  "Perform database migrations that haven't been applied"
+  []
   (migratus/migrate
+    ; Get our database configuration
     (let [db-config (gather-db-config env)]
-      (-> {:store :database
-           :migration-dir "migrations/"
-           :migration-table-name "migrations"}
-          (assoc :db (migratusify-db-spec (gather-db-config env)))))))
+      ; And build a configuration map used by Migratus
+      {:store :database
+       :migration-dir "migrations/"
+       :migration-table-name "migrations"
+       :db (migratusify-db-spec (gather-db-config env))})))
