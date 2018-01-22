d3551286dffcb1a221d68c81f2650af82e0a5a07
Joshua Heimbach <joshkh@gmail.com>
2018-01-22 13:19:05 +0000

    Use cleaner :im-chan fx

--- src/cljs/bluegenes/sections/regions/events.cljs
+++ src/cljs/bluegenes/sections/regions/events.cljs
@@ -7,32 +7,32 @@
 
 (defn parse-region [region-string]
   (let [parsed (into []
-    (remove
-      (fn [c] (some? (some #{c} #{":" ".." "-"})))
-      (clojure.string/split region-string (re-pattern "(:|\\.\\.|-)"))))]
+                     (remove
+                       (fn [c] (some? (some #{c} #{":" ".." "-"})))
+                       (clojure.string/split region-string (re-pattern "(:|\\.\\.|-)"))))]
     {:chromosome (nth parsed 0)
-     :from       (int (nth parsed 1))
-     :to         (int (nth parsed 2))}))
+     :from (int (nth parsed 1))
+     :to (int (nth parsed 2))}))
 
 (reg-event-db
   :regions/save-results
   (fn [db [_ result-response]]
-    (let [searched-for   (get-in db [:regions :regions-searched])
-          error          (:error result-response)
+    (let [searched-for (get-in db [:regions :regions-searched])
+          error (:error result-response)
           mapped-results (map
-            (fn [region]
-                  (assoc region :results (filter (fn [{{:keys [start end locatedOn] :as t} :chromosomeLocation}]
-                   (and
-                     (= (:primaryIdentifier locatedOn) (:chromosome region))
-                     (or
-                       (< (:from region) (int start) (:to region))
-                       (< (:from region) (int end) (:to region)))))
-                 (:results result-response)))
-            ) searched-for)]
+                           (fn [region]
+                             (assoc region :results (filter (fn [{{:keys [start end locatedOn] :as t} :chromosomeLocation}]
+                                                              (and
+                                                                (= (:primaryIdentifier locatedOn) (:chromosome region))
+                                                                (or
+                                                                  (< (:from region) (int start) (:to region))
+                                                                  (< (:from region) (int end) (:to region)))))
+                                                            (:results result-response)))
+                             ) searched-for)]
       (->
-       (assoc-in db [:regions :results] mapped-results)
-       (assoc-in    [:regions :loading] false)
-       (assoc-in    [:regions :error]   error)))))
+        (assoc-in db [:regions :results] mapped-results)
+        (assoc-in [:regions :loading] false)
+        (assoc-in [:regions :error] error)))))
 
 
 (reg-event-db
@@ -44,10 +44,10 @@
 (reg-event-db
   :regions/toggle-feature-type
   (fn [db [_ class]]
-    (let [class-kw    (keyword (:name class))
-          m           (get-in db [:mines (get db :current-mine) :service :model])
+    (let [class-kw (keyword (:name class))
+          m (get-in db [:mines (get db :current-mine) :service :model])
           descendants (keys (entity/extended-by m class-kw))
-          status      (get-in db [:regions :settings :feature-types class-kw])]
+          status (get-in db [:regions :settings :feature-types class-kw])]
       (update-in db [:regions :settings :feature-types]
                  merge
                  (reduce (fn [total next]
@@ -57,7 +57,7 @@
 (reg-event-db
   :regions/select-all-feature-types
   (fn [db]
-    (let [model         (get-in db [:mines (get db :current-mine) :service :model])
+    (let [model (get-in db [:mines (get db :current-mine) :service :model])
           feature-types (entity/extended-by model :SequenceFeature)]
       (assoc-in db [:regions :settings :feature-types]
                 (reduce (fn [total [name]]
@@ -67,9 +67,9 @@
   :regions/deselect-all-feature-types
   (fn [db]
     (let [feature-types (get-in db [:regions :settings :feature-types])]
-    (assoc-in db [:regions :settings :feature-types]
-      (reduce (fn [new-map [k v]]
-                (assoc new-map k false)) {} feature-types)))))
+      (assoc-in db [:regions :settings :feature-types]
+                (reduce (fn [new-map [k v]]
+                          (assoc new-map k false)) {} feature-types)))))
 
 (reg-event-db
   :regions/set-to-search
@@ -78,21 +78,21 @@
 
 (defn add-type-constraints [query types]
   (if types
-    (update query :where conj {:path   "SequenceFeature"
-                               :op     "ISA"
+    (update query :where conj {:path "SequenceFeature"
+                               :op "ISA"
                                :values types})
     query))
 
 (defn add-organism-constraint [query short-name]
   (if short-name
-    (update query :where conj {:path   "SequenceFeature.organism.shortName"
-                               :op     "="
+    (update query :where conj {:path "SequenceFeature.organism.shortName"
+                               :op "="
                                :value short-name})
     query))
 
 
 (defn build-feature-query [regions]
-  {:from   "SequenceFeature"
+  {:from "SequenceFeature"
    :select ["SequenceFeature.id"
             "SequenceFeature.name"
             "SequenceFeature.primaryIdentifier"
@@ -100,27 +100,25 @@
             "SequenceFeature.chromosomeLocation.start"
             "SequenceFeature.chromosomeLocation.end"
             "SequenceFeature.chromosomeLocation.locatedOn.primaryIdentifier"]
-   :where  [{:path   "SequenceFeature.chromosomeLocation"
-             :op     "OVERLAPS"
-             :values (if (string? regions) [regions] (into [] regions))}]})
+   :where [{:path "SequenceFeature.chromosomeLocation"
+            :op "OVERLAPS"
+            :values (if (string? regions) [regions] (into [] regions))}]})
 
 
 (reg-event-fx
   :regions/run-query
   (fn [{db :db} [_]]
-    (let [to-search     (clojure.string/split-lines (get-in db [:regions :to-search]))
+    (let [to-search (clojure.string/split-lines (get-in db [:regions :to-search]))
           feature-types (get-in db [:regions :settings :feature-types])
           selected-organism (get-in db [:regions :settings :organism :shortName])
           query (->
                   (add-type-constraints
-                   (build-feature-query to-search)
-                   (map name (keys (filter (fn [[name enabled?]] enabled?) feature-types))))
+                    (build-feature-query to-search)
+                    (map name (keys (filter (fn [[name enabled?]] enabled?) feature-types))))
                   (add-organism-constraint selected-organism))]
-      {:db           (->
-                      (assoc-in db [:regions :regions-searched] (map parse-region to-search))
-                      (assoc-in [:regions :results] {})
-                      (assoc-in [:regions :loading] true))
-       :im-operation {:op         (partial fetch/records
-                                           (get-in db [:mines (get db :current-mine) :service])
-                                           query)
-                      :on-success [:regions/save-results]}})))
+      {:db (->
+             (assoc-in db [:regions :regions-searched] (map parse-region to-search))
+             (assoc-in [:regions :results] {})
+             (assoc-in [:regions :loading] true))
+       :im-chan {:chan (fetch/records (get-in db [:mines (get db :current-mine) :service]) query)
+                 :on-success [:regions/save-results]}})))
