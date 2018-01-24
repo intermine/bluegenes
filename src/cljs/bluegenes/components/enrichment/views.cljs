4eedd428ffc9e7517cea08625f9feb5aabb420de
Joshua Heimbach <joshkh@gmail.com>
2018-01-24 14:41:28 +0000

    Add chevron SVG icons and use in enrichment tables

--- src/cljs/bluegenes/components/enrichment/views.cljs
+++ src/cljs/bluegenes/components/enrichment/views.cljs
@@ -91,12 +91,12 @@
               ;;[:svg.icon.icon-circle-left [:use {:xlinkHref "#icon-circle-left"}]]
               [:span
                {:on-click (fn [] (swap! page-state update :page dec))
-                :title "View previous 5 enrichment results"} "<"]
+                :title "View previous 5 enrichment results"} [:svg.icon.icon-chevron-left [:use {:xlinkHref "#icon-chevron-left"}]]]
                 ;;TODO: replace the > below with the svg icon when enrichment is fixed.
                 ;;[:svg.icon.icon-circle-right [:use {:xlinkHref "#icon-circle-right"}]]
               [:span
                {:on-click (fn [] (swap! page-state update :page inc))
-                :title "View next 5 enrichment results"}">"]])]
+                :title "View next 5 enrichment results"} [:svg.icon.icon-chevron-right [:use {:xlinkHref "#icon-chevron-right"}]]]])]
           [:span [mini-loader "tiny"]])]
        (cond (seq (:results details)) enrichment-results-header)
        (into [:ul.enrichment-list]
