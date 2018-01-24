e725b4c30caed671d22c614df30ed79e2534a653
Joshua Heimbach <joshkh@gmail.com>
2018-01-24 13:55:02 +0000

    Fix ReactJS key clash that was preventing some table rows from rendering

--- src/cljs/bluegenes/components/ui/results_preview.cljs
+++ src/cljs/bluegenes/components/ui/results_preview.cljs
@@ -34,9 +34,9 @@
           [:tr
            [:td {:col-span (count (:columnHeaders query-results))}
             [:h4 "Query returned no results"]]]
-          (doall (map
-                   (fn [r]
-                     ^{:key (reduce str (map :id r))} [table-row r])
+          (doall (map-indexed
+                   (fn [idx r]
+                     ^{:key idx} [table-row r])
                    (:results query-results))))
         (if (and (not hide-count?) (> (:iTotalRecords query-results) 5))
           [:tr
