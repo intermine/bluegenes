8070970ae6bea49c4a6a455dccff293a276c9ad8
Yo Yehudi <yochannah@gmail.com>
2018-09-14 13:19:30 +0100

    comment tools api route better

--- src/clj/bluegenes/ws/tools.clj
+++ src/clj/bluegenes/ws/tools.clj
@@ -8,16 +8,24 @@
             [ring.util.http-response :as response]
             [clojure.java.io :as io]))
 
-(defn get-tool-config [tool path]
+(defn get-tool-config
+  "check tool folder for config and other relevant files and return as
+   a map of useful info. This is used client-side by the browser to
+   load tools relevant for a given report page."
+  [tool path]
   (let [name (.getName (io/file tool))
         path (join "/" [path name])
+        ;; this is the bluegenes-specific config.
         bluegenes-config (str path "/config.json")
+        ;;this is the default npm package file
         package (str path "/package.json")
+        ;;optional preview image for each tool.
         preview-image "/preview.png"
         browser-path (str "/tools/" name preview-image)]
     {:name name
       :config (cheshire/parse-string (slurp bluegenes-config))
       :package (cheshire/parse-string (slurp package))
+      ;; return image path if it exists, or false otherwise.
       :hasimage (if (.exists (io/file (str path preview-image)))
                   browser-path
                   false)
