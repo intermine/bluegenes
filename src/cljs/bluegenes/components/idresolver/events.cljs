7e5562851c2206a7cf4f1f052643901bb961a87c
Yo Yehudi <yo@intermine.org>
2019-04-26 12:59:42 +0100

    attempt to combat excessive line length * 2

--- src/cljs/bluegenes/components/idresolver/events.cljs
+++ src/cljs/bluegenes/components/idresolver/events.cljs
@@ -30,21 +30,27 @@
    {:db (assoc-in db [:idresolver :stage :status] {:action :parsing})
     ::fx/http {:uri "/api/ids/parse"
                :method :post
-               :multipart-params (cond-> (map (fn [f] [(oget f :name) f]) js-Files)
-                                                      ; After creating multipart params for files, create one for text
-                                                      ; if there's a value
-                                   (not (string/blank? text)) (conj ["text" text])
-                                                      ; And also a param for the case sensitive option
-                                   true (conj ["caseSensitive" (:case-sensitive options)]))
+               :multipart-params
+               (cond->
+                (map (fn [f] [(oget f :name) f])
+                     js-Files)
+                  ; After creating multipart params for files,
+                  ; create one for text
+                  ; if there's a value
+                 (not (string/blank? text))
+                 (conj ["text" text])
+                  ; And also a param for the case sensitive option
+                 true (conj ["caseSensitive" (:case-sensitive options)]))
                :on-success [::store-parsed-response options]}}))
 
 (reg-event-fx
  ::store-parsed-response
  (fn [{db :db} [_ options response]]
-   {:db (update db :idresolver #(-> %
-                                    (assoc-in [:stage :status :action] nil)
-                                    (assoc-in [:stage :flags :parsed] true)
-                                    (assoc :to-resolve response)))
+   {:db (update db :idresolver
+                #(-> %
+                     (assoc-in [:stage :status :action] nil)
+                     (assoc-in [:stage :flags :parsed] true)
+                     (assoc :to-resolve response)))
     :dispatch [::resolve-identifiers options (:identifiers response)]}))
 
 (reg-event-fx
@@ -87,12 +93,15 @@
      (-> db
          (assoc-in [:idresolver :response] response)
          (assoc-in [:idresolver :stage :view] :review)
-         (assoc-in [:idresolver :save :list-name]
-                   (str type
-                        " list for "
-                        (if (string/blank? organism) "all organisms" organism)
-                        " "
-                        (time-format/unparse time-formatter (time/now))))))))
+         (assoc-in
+          [:idresolver :save :list-name]
+          (str type
+               " list for "
+               (if (string/blank? organism)
+                 "all organisms"
+                 organism)
+               " "
+               (time-format/unparse time-formatter (time/now))))))))
 
 (reg-event-db
  ::set-view
@@ -107,17 +116,25 @@
 (reg-event-db
  ::update-type
  (fn [db [_ model value]]
-   (let [disable-organism? (when (not= value "_") (not (contains? (path/relationships model value) :organism)))
+   (let [disable-organism? (when
+                            (not= value "_")
+                             (not (contains?
+                                   (path/relationships model value)
+                                   :organism)))
          mine-details      (get-in db [:mines (get db :current-mine)])]
      (if disable-organism?
-       (update-in db [:idresolver :stage :options] assoc :type value :disable-organism? true :organism nil)
-       (update-in db [:idresolver :stage :options] #(cond-> %
-                                                                         ; Assoc the new Type value to the options
-                                                      value (assoc :type value)
-                                                                         ; Enabled organism dropdown
-                                                      true (assoc :disable-organism? false)
-                                                                         ; Give the organism dropdown a value if nil
-                                                      (nil? (:organism %)) (assoc :organism (-> mine-details :default-organism))))))))
+       (update-in db [:idresolver :stage :options]
+                  assoc :type value :disable-organism? true :organism nil)
+       (update-in db [:idresolver :stage :options]
+                  #(cond-> %
+                      ; Assoc the new Type value to the options
+                     value (assoc :type value)
+                      ; Enabled organism dropdown
+                     true (assoc :disable-organism? false)
+                      ; Give the organism dropdown a value if nil
+                     (nil? (:organism %))
+                     (assoc :organism
+                            (-> mine-details :default-organism))))))))
 
 (reg-event-db
  ::update-option
@@ -127,7 +144,9 @@
 (reg-event-db
  ::toggle-keep-duplicate
  (fn [db [_ duplicate-idx match-idx]]
-   (update-in db [:idresolver :response :matches :DUPLICATE duplicate-idx :matches match-idx :keep?] not)))
+   (update-in db [:idresolver :response
+                  :matches :DUPLICATE duplicate-idx
+                  :matches match-idx :keep?] not)))
 
 (reg-event-db
  ::update-list-name
@@ -142,41 +161,50 @@
          object-type (get-in db [:idresolver :stage :options :type])
          service     (get-in db [:mines (get db :current-mine) :service])
          list-name   (get-in db [:idresolver :save :list-name])]
-     {:im-chan {:chan
-                (save/im-list-from-query
-                 service
-                 list-name
-                 {:from object-type
-                  :select [(str object-type ".id")]
-                  :where [{:path (str object-type ".id")
-                           :op "ONE OF"
-                           :values (->> MATCH
-                                        (concat (mapcat :matches OTHER))
-                                        (concat (mapcat :matches TYPE_CONVERTED))
-                                        (concat (mapcat (fn [{matches :matches}] (filter :keep? matches)) DUPLICATE))
-                                        (map :id))}]})
-                :on-success [::save-list-success list-name object-type]}})))
+     {:im-chan
+      {:chan
+       (save/im-list-from-query
+        service
+        list-name
+        {:from object-type
+         :select [(str object-type ".id")]
+         :where [{:path (str object-type ".id")
+                  :op "ONE OF"
+                  :values (->>
+                           MATCH
+                           (concat (mapcat :matches OTHER))
+                           (concat (mapcat :matches TYPE_CONVERTED))
+                           (concat
+                            (mapcat (fn [{matches :matches}]
+                                      (filter :keep? matches)) DUPLICATE))
+                           (map :id))}]})
+       :on-success [::save-list-success list-name object-type]}})))
 
 (reg-event-fx
  ::save-list-success
  (fn [{db :db} [_ list-name object-type response]]
-                ; Get the summary fields for this object type (used to construct the query for the List Analysis page
+   ; Get the summary fields for this object type (used to construct the
+   ;query for the List Analysis page
    (let [{:keys [listName]} response
-         summary-fields (get-in db [:assets :summary-fields (get db :current-mine) (keyword object-type)])]
+         summary-fields
+         (get-in db
+                 [:assets :summary-fields
+                  (get db :current-mine) (keyword object-type)])]
      {:db db
                    ; Navigate to the List Analysis page
                    ;:navigate "results"
       :dispatch-n [; Re-fetch our lists so that it shows in MyMine
                    [:assets/fetch-lists]
                                 ; Show the list results in the List Analysis page
-                   [:results/history+ {:source (get db :current-mine)
-                                       :type :query
-                                       :value {:title list-name
-                                               :from object-type
-                                               :select summary-fields
-                                               :where [{:path object-type
-                                                        :op "IN"
-                                                        :value list-name}]}}]]})))
+                   [:results/history+
+                    {:source (get db :current-mine)
+                     :type :query
+                     :value {:title list-name
+                             :from object-type
+                             :select summary-fields
+                             :where [{:path object-type
+                                      :op "IN"
+                                      :value list-name}]}}]]})))
 
 (reg-event-db
  ::reset
@@ -186,7 +214,9 @@
             {:stage {:files nil
                      :textbox example-text
                      :options {:case-sensitive false
-                               :type (or example-type (-> mine-details :default-object-types first name))
+                               :type (or example-type
+                                         (-> mine-details
+                                             :default-object-types first name))
                                :organism (-> mine-details :default-organism)}
                      :status nil
                      :flags nil}
@@ -195,5 +225,8 @@
 (reg-event-fx
  ::load-example
  (fn [{db :db} [_]]
-   (let [[t e] (first (seq (get-in db [:mines (get db :current-mine) :idresolver-example])))]
+   (let [[t e] (first
+                (seq
+                 (get-in db [:mines
+                             (get db :current-mine) :idresolver-example])))]
      {:dispatch [::reset (name t) e]})))
