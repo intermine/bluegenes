e94bee65e2b26d0c62fba01e8a2674ee38b98413
uosl <regen@anche.no>
2019-11-15 15:52:12 +0000

    Docstrings and unit tests!

--- src/cljs/bluegenes/pages/mymine/views/organize.cljs
+++ src/cljs/bluegenes/pages/mymine/views/organize.cljs
@@ -10,19 +10,28 @@
 ;; Tree Structure ;;
 ;;;;;;;;;;;;;;;;;;;;
 
-(defn read-path [tags]
+(defn read-path
+  "Takes a sequence of tags and returns the first tag starting with
+  `path-tag-prefix` and a colon, as a vector split at each '.' char."
+  [tags]
   (let [re (re-pattern (str path-tag-prefix ":(.+)"))]
     (some-> (->> tags
                  (some #(re-find re %))
                  second)
             (string/split #"\."))))
 
-(defn tree-path [path]
+(defn tree-path
+  "Takes a vector of strings and returns the corresponding tree path as a
+  vector of keywords and strings. This path will be the location of the list
+  with specified tag, in a tree implemented as nested maps."
+  [path]
   (apply concat [(when path [:folders])
                  (interpose :folders path)
                  [:lists]]))
 
-(defn lists->tree [lists]
+(defn lists->tree
+  "Pass a sequence of list maps and receive a complete tree."
+  [lists]
   (reduce (fn [tree {:keys [tags title] :as l}]
             (let [path (read-path tags)
                   ks   (tree-path path)
@@ -32,6 +41,7 @@
           lists))
 
 (defn tree->tags
+  "Returns a map of list titles and their new path tags, as computed from the tree."
   ([tree] (tree->tags [] tree))
   ([path tree]
    (into {}
@@ -47,32 +57,48 @@
 ;; Logic ;;
 ;;;;;;;;;;;
 
-(defn list-path? [p]
+(defn list-path?
+  "Whether `p` is a path to a list."
+  [p]
   (= (get p (- (count p) 2))
      :lists))
 
-(defn direct-parent-of? [p c]
+(defn direct-parent-of?
+  "Path `p` is a direct parent of path `c`."
+  [p c]
   (= p (drop-last 2 c)))
 
-(defn child-of? [c p]
+(defn child-of?
+  "Path `c` is a child (nested somewhere within) path `p`."
+  [c p]
   (string/starts-with? (string/join "." c)
                        (string/join "." p)))
 
-(defn top-node? [p]
+(defn top-node?
+  "Path `p` is a node at the top of the tree, meaning its parent is the root node."
+  [p]
   (= (count p) 2))
 
-(defn root-node? [p]
+(defn root-node?
+  "Whether `p` is the path to the root node."
+  [p]
   (= p []))
 
-(defn being-dragged? [path dragging]
+(defn being-dragged?
+  "Predicate for whether `path` is the target of `dragging`."
+  [path dragging]
   (= path dragging))
 
-(defn being-dropped? [path dropping]
+(defn being-dropped?
+  "Predicate for whether `path` is the target of `dropping`."
+  [path dropping]
   (or (= path dropping)
       (and (list-path? dropping)
            (= path (drop-last 2 dropping)))))
 
-(defn valid-drop? [dragging dropping]
+(defn valid-drop?
+  "Predicate for whether dropping paths `dragging` into `dropping` is a valid action."
+  [dragging dropping]
   (let [dropping (if (list-path? dropping)
                    (drop-last 2 dropping)
                    dropping)]
@@ -88,6 +114,7 @@
 ;; https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API#Drag_Events
 
 (defn drag-events
+  "Event attributes for a draggable node."
   [state item]
   {:draggable true
 
@@ -100,6 +127,7 @@
                     (swap! state assoc :drag nil))})
 
 (defn drop-events
+  "Event attributes for a droppable node."
   [state item]
   {:on-drag-over  (fn [evt]
                     (ocall evt :preventDefault)
@@ -127,6 +155,10 @@
                                                   (update-in (drop-last dragging) dissoc (last dragging)))))))))))})
 
 (defn cancel-drop-events
+  "Event attributes for an element whose traversal nullifies the dropping target.
+  We use this for the container element, which has a padding that lies outside
+  any of the drag & drop child elements, so we can safely cancel any drop targets
+  when the user moves into this area."
   [state]
   {:on-drag-enter (fn [evt]
                     (ocall evt :stopPropagation)
@@ -136,7 +168,12 @@
 ;; DOM Elements ;;
 ;;;;;;;;;;;;;;;;;;
 
-(defn node [state {:keys [title icon last? on-click path fake?]} & children]
+(defn node
+  "This can be a folder or a list, which you can interact with through
+  drag and drop. Note that a node can also be 'fake' or 'hidden', in which
+  case they're non-interactive and used as a placeholder to signify a valid
+  drop target."
+  [state {:keys [title icon last? on-click path fake?]} & children]
   (let [{:keys [dragging dropping]} (:drag @state)
         drag? (and (being-dragged? path dragging)
                    (not (valid-drop? dragging dropping)))
@@ -156,12 +193,17 @@
              [:span.drag-handle "☰"]]]]
           children)))
 
-(defn root-node [state]
+(defn root-node
+  "This is a special root node for use as a drop target to move nested nodes
+  into the top level."
+  [state]
   [:li.node-container.node-fake
     [:div.node-parent (drop-events state [])
      [:div.node.root (drag-events state [])]]])
 
-(defn list-node [state path [_ {:keys [title fake?]}] & {:keys [last?]}]
+(defn list-node
+  "Wrapper around node for lists."
+  [state path [_ {:keys [title fake?]}] & {:keys [last?]}]
   [node state {:title title
                :icon "list"
                :last? last?
@@ -169,7 +211,9 @@
                :fake? fake?}])
 
 (declare level)
-(defn folder-node []
+(defn folder-node
+  "Wrapper around node for folders."
+  []
   (let [open? (r/atom true)]
     (fn [state path [title children] & {:keys [last?]}]
       [node state {:title title
@@ -181,7 +225,10 @@
        (when (and @open? (not-empty children))
            [level state path children])])))
 
-(defn map->node [state path [title data] & {:keys [last?]}]
+(defn map->node
+  "Takes a map corresponding to either a list or folder, as read from the tree,
+  and returns the correct node element."
+  [state path [title data] & {:keys [last?]}]
   (let [folder? (not (contains? data :tags))
         path'   (if folder?
                   (conj path :folders title)
@@ -191,7 +238,15 @@
      state path' [title data]
      :last? last?]))
 
-(defn level [state path {:keys [lists folders]} & {:keys [top?]}]
+(defn level
+  "This corresponds to one level of nesting within the tree. Every time there's
+  a folder, we nest once more. Hence this function is mutually recursive with
+  `folder-node`. As you can see from the comments in the function body, there
+  are 3 nodes added manually in addition to the 'real' nodes:
+  - Placeholder node to signify a valid drop target at this level
+  - Top level root node (see `root-node`)
+  - Top level placeholder node for when the root node is the drop target"
+  [state path {:keys [lists folders]} & {:keys [top?]}]
   (-> [:ul {:className (if top? "top-level-list" "list")}]
       (into (let [items (concat folders lists
                                 ;; Append a placeholder node when the user hovers over
@@ -228,7 +283,9 @@
 ;; TODO may only contain "letters, numbers, spaces, hyphens and colons.",
 ;; "full stops" are technically permitted but we use them for nesting
 ;; TODO also check that folder name doesn't already exist
-(defn new-folder [state]
+(defn new-folder
+  "Input and button for creating a new folder."
+  [state]
   [:div.node-parent
    [:div.node.node-input
     [:svg.icon.icon-folder-plus [:use {:xlinkHref "#icon-folder-plus"}]]
@@ -248,7 +305,9 @@
      "Add"]]])
 
 ;; TODO make sure that this is only run when modal is open (? is this really a good idea)
-(defn main [state]
+(defn main
+  "Main function for creating an organize element."
+  [state]
   (let [lists (subscribe [:lists/filtered-lists])]
     (swap! state assoc :tree (lists->tree @lists))
     (fn [state]
---
+++ test/cljs/bluegenes/organize_test.cljs
@@ -0,0 +1,135 @@
+(ns bluegenes.organize-test
+  (:require [cljs.test :refer-macros [deftest is are testing]]
+            [bluegenes.pages.mymine.views.organize :as organize]))
+
+(defn tag
+  "Helper function to create a compliant path tag."
+  [& [s]]
+  (str organize/path-tag-prefix ":" s))
+
+(deftest read-path
+  (is (= (organize/read-path ["idc" "im:public" (tag "foo.bar") "bgother:ignore"])
+         ["foo" "bar"]))
+  (is (= (organize/read-path ["idc" "im:public" "bgother:ignore"])
+         nil)))
+
+(deftest tree-path
+  (is (= (organize/tree-path nil)
+         [:lists]))
+  (is (= (organize/tree-path ["less"])
+         [:folders "less" :lists]))
+  (is (= (organize/tree-path ["less" "more"])
+         [:folders "less" :folders "more" :lists])))
+
+(let [list-a {:tags [] :title "list-a" :id 0}
+      list-b {:tags [(tag "less")] :title "list-b" :id 1}
+      list-c {:tags [(tag "less")] :title "list-c" :id 2}
+      list-d {:tags [(tag "less.deeper")] :title "list-d" :id 3}
+      list-e {:tags [(tag "more")] :title "list-e" :id 4}
+      tree {:lists {"list-a" list-a}
+            :folders {"less" {:lists {"list-b" list-b
+                                      "list-c" list-c}
+                              :folders {"deeper" {:lists {"list-d" list-d}}}}
+                      "more" {:lists {"list-e" list-e}}}}]
+
+  (deftest lists->tree
+    (is (= (organize/lists->tree [list-a list-b list-c list-d list-e])
+           tree)))
+
+  (deftest tree->tags
+    (is (= (organize/tree->tags tree)
+           {"list-a" (tag)
+            "list-b" (tag "less")
+            "list-c" (tag "less")
+            "list-d" (tag "less.deeper")
+            "list-e" (tag "more")}))))
+
+(deftest list-path?
+  (is (false? (organize/list-path? [])))
+  (is (false? (organize/list-path? [:folders "foo" :folders "bar"])))
+  (is (true?  (organize/list-path? [:folders "foo" :lists "bar"]))))
+
+(deftest direct-parent-of?
+  (is (true? (organize/direct-parent-of?
+               [:folders "one" :folders "two"]
+               [:folders "one" :folders "two" :lists "foo"])))
+  (is (true? (organize/direct-parent-of?
+               [:folders "one"]
+               [:folders "one" :lists "foo"])))
+  (is (true? (organize/direct-parent-of?  [] [:lists "foo"])))
+  (is (false? (organize/direct-parent-of?
+                [:folders "one" :folders "two"]
+                [:folders "one" :folders "two"])))
+  (is (false? (organize/direct-parent-of?
+                [:folders "one"]
+                [:folders "one" :folders "two" :lists "foo"]))))
+
+(deftest child-of?
+  (is (true? (organize/child-of? [:folders "one" :folders "two" :lists "foo"]
+                                 [:folders "one" :folders "two"])))
+  (is (true? (organize/child-of? [:folders "one" :folders "two" :lists "foo"]
+                                 [:folders "one"])))
+  (is (false? (organize/child-of? [:folders "one" :lists "foo"]
+                                  [:folders "one" :folders "two"]))))
+
+(deftest top-node?
+  (is (true? (organize/top-node? [:lists "foo"])))
+  (is (true? (organize/top-node? [:folders "more"])))
+  (is (false? (organize/top-node? [:folders "more" :lists "foo"])))
+  (is (false? (organize/top-node? []))))
+
+(deftest root-node?
+  (is (false? (organize/root-node? [:folders "more"])))
+  (is (true? (organize/root-node? []))))
+
+(deftest being-dragged?
+  (is (true? (organize/being-dragged? [:folders "less"] [:folders "less"])))
+  (is (false? (organize/being-dragged? [:folders "less"] [:folders "more"]))))
+
+(deftest being-dropped?
+  (is (true? (organize/being-dropped? [:folders "less"] [:folders "less"])))
+  (is (true? (organize/being-dropped? [:folders "less"]
+                                      [:folders "less" :lists "foo"])))
+  (is (false? (organize/being-dropped? [:folders "less"]
+                                       [:folders "less" :folders "foo"]))))
+
+(deftest valid-drop?
+  (is (true? (organize/valid-drop?
+               [:lists "Z7 Special"]
+               [:folders "TFG" :folders "more" :lists "TFG Panel Extra"])))
+  (is (true? (organize/valid-drop?
+               [:lists "Z7 Special"]
+               [:folders "TFG" :folders "more"])))
+  (is (true? (organize/valid-drop?
+               [:folders "TFG" :folders "more"]
+               [:folders "TFG" :folders "less" :lists "TFG Panel Super"])))
+  (is (true? (organize/valid-drop?
+               [:folders "TFG" :folders "less" :folders "more" :lists "Z7 Special"]
+               [])))
+  (is (true? (organize/valid-drop?
+               [:folders "TFG" :folders "less"]
+               [])))
+  (is (false? (organize/valid-drop?
+                [:folders "TFG" :lists "TFG Panel Multi"]
+                [:folders "TFG" :lists "TFG Panel Blue"])))
+  (is (false? (organize/valid-drop?
+                [:folders "less" :lists "TFG Panel Super"]
+                [:folders "less"])))
+  (is (false? (organize/valid-drop?
+                [:folders "TFG"]
+                [])))
+  (is (false? (organize/valid-drop?
+                [:lists "Z7 Special"]
+                [])))
+  (is (false? (organize/valid-drop?
+                [:folders "less"]
+                [:lists "Z7 Special"])))
+  (is (false? (organize/valid-drop?
+                [:folders "less"]
+                [:folders "less" :folders "more"])))
+  (is (false? (organize/valid-drop?
+                [:folders "less"]
+                [:folders "less" :lists "TFG Panel Super"])))
+  (is (false? (organize/valid-drop?
+                [:folders "less"]
+                [:folders "less" :folders "more" :lists "TFG Panel Extra"]))))
