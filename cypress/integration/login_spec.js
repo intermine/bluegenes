64c92ee0d6b174b09774da566732d3cf03936cbf
Amy Tai <amy.tai0120@gmail.com>
2020-03-27 20:19:29 -0700

    fixed weird spacings

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,29 +1,29 @@
-describe("Login Tests", function() {
+describe("Authentication Tests", function() {
 	beforeEach(function() {
 		cy.visit("/");
 
 		cy.server();
-    cy.route("POST", "/api/auth/login").as("auth");
-    cy.contains("Log In").click();
+		cy.route("POST", "/api/auth/login").as("auth");
+		cy.contains("Log In").click();
 	});
-	
-  it("login and logout user successfully", function() {
-    cy.get("#email").type("test_user@mail_account");
-    cy.get("input[type='password']").type("secret");
-    cy.get("form")
-      .find("button")
-      .click();
-    cy.wait("@auth");
-    cy.get("@auth").should(xhr => {
-      expect(xhr.status).to.equal(200);
-    });
+		
+	it("login and logout user successfully", function() {
+		cy.get("#email").type("test_user@mail_account");
+		cy.get("input[type='password']").type("secret");
+		cy.get("form")
+			.find("button")
+			.click();
+		cy.wait("@auth");
+		cy.get("@auth").should(xhr => {
+			expect(xhr.status).to.equal(200);
+		});
 
-    cy.contains("test_user@mail_account").should('be.visible');
+		cy.contains("test_user@mail_account").should('be.visible');
 
-    cy.get('#bluegenes-main-nav .logon').click();
-    cy.get('#bluegenes-main-nav .logon').contains('Log Out').click();
+		cy.get('#bluegenes-main-nav .logon').click();
+		cy.get('#bluegenes-main-nav .logon').contains('Log Out').click();
 	});
-	
+
 	// **The response for the two tests below give me inconsistent response; sometimes 500, other times, 401.
 	// Uncomment when resolved.
 	// it("requires an email input; expect error", () => {
