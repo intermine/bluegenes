656fdc7ce7d07a1724160a21ffcb08f8f5c7701d
uosl <regen@anche.no>
2020-06-15 17:52:36 +0100

    Autoformat code

--- src/cljs/bluegenes/pages/mymine/views/mymine.cljs
+++ src/cljs/bluegenes/pages/mymine/views/mymine.cljs
@@ -98,8 +98,8 @@
           (let [no-login? (not @(subscribe [:bluegenes.subs.auth/authenticated?]))
                 no-lists? (empty? @(subscribe [:lists/authorized-lists]))
                 no-support? (not (utils/compatible-version?
-                                   version/organize-support
-                                   @(subscribe [:current-intermine-version])))
+                                  version/organize-support
+                                  @(subscribe [:current-intermine-version])))
                 problem? (or no-lists? no-login? no-support?)]
             [:li.hidden-xs {:class (when problem? "disabled")}
              [:a (merge
