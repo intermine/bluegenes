e5267faa05e3773c8776105abe30d5b5f7571428
uosl <regen@anche.no>
2020-08-12 16:09:52 +0100

    Tools: Small styling and markup change to accomodate current tools

--- src/cljs/bluegenes/components/tools/views.cljs
+++ src/cljs/bluegenes/components/tools/views.cljs
@@ -13,6 +13,9 @@
                         ;; Most tools have a name starting with "BlueGenes"
                         ;; which is frankly not very useful, so we remove it.
                         pretty-name (str/replace human #"(?i)^bluegenes\s*" "")]]
+              ;; Please do not change the markup that follows. Its structure is
+              ;; copied to each tool's demo.html so they get a similar styling.
+              ;; However, you are free to change the contents of div.tool-header
               [:div.tool-container
                [:div.tool-header
                 [:h3.tool-title pretty-name]
@@ -21,4 +24,5 @@
                               #(dispatch [::events/expand-tool cljs])
                               #(dispatch [::events/collapse-tool cljs]))}
                  (if collapsed? "＋" "—")]]
-               [:div.tool {:id cljs :class [cljs (when collapsed? :hidden)]}]])))))
+               [:div.tool {:class [cljs (when collapsed? :hidden)]}
+                [:div {:id cljs}]]])))))
