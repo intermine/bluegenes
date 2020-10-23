d76033abfa2ee9d3858bb760eed943831b037b49
uosl <regen@anche.no>
2020-10-23 16:49:41 +0100

    Report: Styling and example for datasources in sidebar

--- src/cljs/bluegenes/pages/reportpage/components/sidebar.cljs
+++ src/cljs/bluegenes/pages/reportpage/components/sidebar.cljs
@@ -1,7 +1,8 @@
 (ns bluegenes.pages.reportpage.components.sidebar
   (:require [re-frame.core :refer [subscribe dispatch]]
             [reagent.core :as reagent]
-            [bluegenes.components.icons :refer [icon-comp]]))
+            [bluegenes.components.icons :refer [icon-comp]]
+            [bluegenes.components.bootstrap :refer [poppable]]))
 
 (defn main []
   [:div.sidebar
@@ -16,7 +17,17 @@
      [:li [:a "Humanmine"]]
      [:li [:a "Flymine"]]]]
    [:div.sidebar-entry
+    [:h4 "Data sources"]
+    [:ul
+     [:li [poppable {:data "Gene family assignments for glyma.Wm82.gnm2 genes and proteins."
+                     :children [:a
+                                {:href "https://legumeinfo.org/data/public/Glycine_max/Wm82.gnm2.ann1.RVB6/"
+                                 :target "_blank"}
+                                "glyma.Wm82.gnm2.ann1.RVB6.legfed_v1_0.M65K.gfa.tsv"
+                                [:div.icon-background
+                                 [icon-comp "external"]]]}]]]]
+   [:div.sidebar-entry
     [:h4 "External resources"]
     [:ul
-     [:li [:a "Humanmine"]]
-     [:li [:a "Flymine"]]]]])
+     [:li [:a "Ensembl"]]
+     [:li [:a "BioGRID"]]]]])
