8038db8c4e3ac47d073b1e118f7e642e5f5ccbeb
uosl <regen@anche.no>
2020-11-23 15:05:37 +0000

    Fix importing im-tables.css failing due to missing parent dir

--- src/clj/bluegenes/prep.clj
+++ src/clj/bluegenes/prep.clj
@@ -5,9 +5,13 @@
   (:require [clojure.java.io :as io]))
 
 (defn copy-im-tables-css []
-  (->> (io/resource "public/css/im-tables.css")
-       (slurp)
-       (spit "resources/public/css/im-tables.css")))
+  (let [source-file "public/css/im-tables.css"
+        target-file "resources/public/css/im-tables.css"]
+    ;; css dir is missing after a lein clean.
+    (io/make-parents target-file)
+    (->> (io/resource source-file)
+         (slurp)
+         (spit target-file))))
 
 (defn prepare-assets []
   (copy-im-tables-css))
