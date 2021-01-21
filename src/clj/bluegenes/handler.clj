86708b78ccb0671946e613503765f8a074513773
uosl <regen@anche.no>
2021-01-21 12:28:59 +0000

    Fix routes with trailing slash by redirecting if present
    Fixes #516

--- src/clj/bluegenes/handler.clj
+++ src/clj/bluegenes/handler.clj
@@ -9,7 +9,20 @@
             [ring.middleware.reload :refer [wrap-reload]]
             [ring.middleware.format :refer [wrap-restful-format]]
             [compojure.core :refer [routes]]
+            [compojure.middleware :refer [wrap-canonical-redirect]]
             [bluegenes-tool-store.core :as tool]))
+
+(defn remove-trailing-slash
+  "Remove the trailing '/' from a URI string, if it exists and isn't the only character."
+  [^String uri]
+  (if (.endsWith uri "/")
+    (let [len (.length uri)]
+      ;; Without this check, the response becomes 301 with a blank Location
+      ;; header for request uri path "/", causing nothing to be loaded.
+      (if (> len 1)
+        (.substring uri 0 (dec len))
+        uri))
+    uri))
 
 (def combined-routes
   (routes tool/routes routes/routes))
@@ -20,4 +33,7 @@
                  ; Add session functionality to the Ring requests
                  wrap-session
                  ; Accept and parse request parameters in various formats
-                 (wrap-restful-format :formats [:json :json-kw :transit-msgpack :transit-json])))
+                 (wrap-restful-format :formats [:json :json-kw :transit-msgpack :transit-json])
+                 ; Redirect to the correct route when trailing slash is present in path.
+                 ; This is for the frontend router, which doesn't handle trailing slashes.
+                 (wrap-canonical-redirect remove-trailing-slash)))
