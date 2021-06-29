bf68c7841381806c671d0a5bd36e2adbf33561eb
jiaq8877 <61627411+jiaq8877@users.noreply.github.com>
2021-06-29 09:46:39 +0100

    Updated templates test

--- cypress/integration/templates_test.js
+++ cypress/integration/templates_test.js
@@ -1,36 +1,35 @@
 describe("Templates Test", function() {
     beforeEach(function() {
-      cy.visit("/");
+      cy.visit("/biotestmine/templates");
     });
 
-    it("can be accessed from the navigation tab", function() {
-        cy.openTemplatesTab();
-        cy.url().should("include", "/templates");
-    });
+    // it("can be accessed from the navigation tab", function() {
+    //     cy.get("#bluegenes-main-nav").within(() => {
+    //         cy.contains("Templates").click();
+    //     });
+    //     cy.url().should("include", "/templates");
+    // });
 
-    it("can be accessed from the main page", function() {
-        cy.contains("More queries here").click();
-        cy.url().should("include", "/templates");
-    });
+    // it("can be accessed from the main page", function() {
+    //     cy.contains("More queries here").click();
+    //     cy.url().should("include", "/templates");
+    // });
 
     it("can filter templates by search", function() {
-        cy.openTemplatesTab();
-        cy.url().should("include", "/templates");
-        cy.get('input[class="form-control input-lg"]').type("Organism{enter}",{delay:100});
-        cy.get('#Organism_Protein').should("have.id","Organism_Protein");
+        // cy.visit("/biotestmine/templates");
+        cy.contains('Filter by description').next().type("Organism{enter}",{delay:100});
+        cy.get('.template').should("have.id","Organism_Protein");
     });
 
     it("can search templates by categories", function() {
-        cy.openTemplatesTab();
-        cy.url().should("include","/templates");
-        cy.get("div[class=grid-1]").should("have.length","3");
+        // cy.visit("/biotestmine/templates");
+        cy.get(".template").should("have.length","3");
         cy.contains("Genomics").click();
-        cy.get("div[class=grid-1]").should("have.length","1");
+        cy.get(".template").should("have.length","1");
     });
 
     it("can load predefined template and show results", function() {
-        cy.openTemplatesTab();
-        cy.url().should("include","/templates");
+        // cy.visit("/biotestmine/templates");
 
         cy.contains("View").first().click();
         cy.url().should("include","/All_Proteins_In_Organism_To_Publications");
