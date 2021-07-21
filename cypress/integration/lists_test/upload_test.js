c80c303e5846bf8a22e05023e43da30d7d575379
jiaq8877 <61627411+jiaq8877@users.noreply.github.com>
2021-07-21 15:06:19 +0100

    Added upload tests

--- cypress/integration/lists_test/upload_test.js
+++ cypress/integration/lists_test/upload_test.js
@@ -9,6 +9,20 @@ describe("Upload Test", function(){
         cy.url().should("include","/save");
     });
 
+    it("can select list type and organism", function(){
+
+        cy.get('.select-organism-and-list-type').within(() => {
+            cy.contains("List type").siblings(".form-group").find("select").select("Exon");
+            // cy.contains("Organism").siblings(".organism-selector").find("select").select("Any");
+        })
+        cy.get('.select-organism-and-list-type > :nth-child(2) > .form-group > .form-control').select("Any"); //to rewrite
+        cy.get('#caseSensitiveCheckbox').check();
+
+        cy.get(".wizard").find("textarea").type("exon.46309,exon.46313",{delay:100});
+        cy.contains("Continue").click();
+        cy.url().should("include","/save");
+    })
+
     it("can upload and save list", function(){
         cy.get(".wizard").find("textarea").type("ABRA, CRK2, CDPK1, CDPK4",{delay:100});
         cy.contains("Continue").click();
@@ -32,4 +46,10 @@ describe("Upload Test", function(){
         cy.contains("Save List").click();
         cy.url().should("include","/results");
     });
+
+    it("can reset to cancel upload", function(){
+        cy.get(".wizard").find("textarea").type("ABRA, CRK2, CDPK1, CDPK4",{delay:100});
+        cy.contains("Reset").click();
+        cy.get(".wizard").find("textarea").should("have.value","");
+    })
 })
