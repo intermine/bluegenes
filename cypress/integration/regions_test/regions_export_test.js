4badd3820069570d7cb2be7fece6e8ced15b2a2d
jiaq8877 <61627411+jiaq8877@users.noreply.github.com>
2021-08-03 16:27:11 +0100

    Added regions export tests

--- cypress/integration/regions_test/regions_export_test.js
+++ cypress/integration/regions_test/regions_export_test.js
@@ -1,6 +1,87 @@
 describe("Regions Export Test", function(){
     beforeEach(function(){
         cy.visit("/biotestmine/regions");
+        cy.get(".input-section").within(() => {
+            cy.contains("Show Example").click();
+            cy.get("textarea").should("include.text","MAL");
+            cy.get("button").filter(':contains("Search")').click();
+        })
     });
+
+    it("can export results of region search in TAB", function(){
+        cy.window().document().then(function (doc) {
+            doc.addEventListener('click', () => {
+              setTimeout(function () { doc.location.reload() }, 5000)
+            })
+            cy.get(".results-actions").within(() => {
+                cy.contains("TAB").click();
+            })
+        })
+        cy.readFile('cypress/downloads/result.tsv').should("contain", "MAL1");
+    })
+
+    it("can export results of region search in CSV", function(){
+        cy.window().document().then(function (doc) {
+            doc.addEventListener('click', () => {
+              setTimeout(function () { doc.location.reload() }, 5000)
+            })
+            cy.get(".results-actions").within(() => {
+                cy.contains("CSV").click();
+            })
+        })
+        cy.readFile('cypress/downloads/result.csv').should("contain", "MAL1");
+    })
+
+    it("can export results of region search in GFF3", function(){
+        cy.intercept('**/service/query/results/**').as('records');
+        cy.window().document().then(function (doc) {
+            doc.addEventListener('click', () => {
+              setTimeout(function () { doc.location.reload() }, 5000)
+            })
+            cy.get(".results-actions").within(() => {
+                cy.contains("GFF3").click();
+            })
+        })
+        cy.wait('@records').its('request').then((req) => {
+            cy.request(req)
+            .then(({ body, headers }) => {
+                cy.readFile("cypress/fixtures/result.gff3").should("eq",body);
+            })
+          })
+    })
+    it("can export results of region search in BED", function(){
+        cy.intercept('**/service/query/results/**').as('records');
+        cy.window().document().then(function (doc) {
+            doc.addEventListener('click', () => {
+              setTimeout(function () { doc.location.reload() }, 5000)
+            })
+            cy.get(".results-actions").within(() => {
+                cy.contains("BED").click();
+            })
+        })
+        cy.wait('@records').its('request').then((req) => {
+            cy.request(req)
+            .then(({ body, headers }) => {
+                cy.readFile("cypress/fixtures/result.bed").should("eq",body);
+            })
+          })
+    })
+    it("can export results of region search in FASTA", function(){
+        cy.intercept('**/service/query/results/**').as('records');
+        cy.window().document().then(function (doc) {
+            doc.addEventListener('click', () => {
+              setTimeout(function () { doc.location.reload() }, 5000)
+            })
+            cy.get(".results-actions").within(() => {
+                cy.contains("FASTA").click();
+            })
+        })
+        cy.wait('@records').its('request').then((req) => {
+            cy.request(req)
+            .then(({ body, headers }) => {
+                cy.readFile("cypress/fixtures/result.fa").should("eq",body);
+            })
+          })
+    })
 
 })
