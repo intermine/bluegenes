4ec63e7ae9d01dd40c81e7f5968d98ebe7c64a9c
jiaq8877 <61627411+jiaq8877@users.noreply.github.com>
2021-08-05 14:28:53 +0100

    Added more assertions to regions tests

--- cypress/integration/regions_test/regions_test.js
+++ cypress/integration/regions_test/regions_test.js
@@ -81,54 +81,85 @@ describe("Regions Test", function(){
     })
 
     it("can extend gene search region by clicking tick marks", function(){
-        cy.get(".extend-region").within(() => {
-            cy.get(".slider-ticks").eq(0).within(() => {
-                cy.contains("1k").click();
-                cy.wait(500);
+        cy.get(".input-section").within(() => {
+            cy.get("textarea").clear().type("MAL1:10000..40000",{delay:100});
+            cy.get(".extend-region").within(() => {
+                cy.get(".slider-ticks").eq(0).within(() => {
+                    cy.contains("1k").click();
+                    cy.wait(500);
+                })
+                cy.get(".form-control").eq(1).should("have.value","1k");
             })
-            cy.get(".form-control").eq(1).should("have.value","1k");
+            cy.get("button").filter(':contains("Search")').click();
+        })
+        cy.get(".results").should("exist").within(() => {
+            cy.get("#region-result-0").should("include.text","MAL1 9000..41000");
         })
     })
 
     it("can extend gene search region by sliders", function(){
         cy.get(".input-section").within(() => {
-            cy.get("textarea").clear().type("MAL1:0..30000",{delay:100});
+            cy.get("textarea").clear().type("MAL1:10000..40000",{delay:100});
+            cy.get(".extend-region").within(() => {
+                cy.get('input[type="range"]').eq(0)
+                .then($el => $el[0].stepUp(40))
+                .trigger('change');
+                cy.wait(500);
+                cy.get(".form-control").eq(1).should("have.value","10k");
+            })
+            cy.get("button").filter(':contains("Search")').click();
         })
-        cy.get(".extend-region").within(() => {
-            cy.get('input[type="range"]').eq(0)
-            .then($el => $el[0].stepUp(40))
-            .trigger('change');
-            cy.wait(500);
-            cy.get(".form-control").eq(1).should("have.value","10k");
+        cy.get(".results").within(() => {
+            cy.get("#region-result-0").should("include.text","MAL1 0..50000");
         })
     })
 
     it("can extend gene search region by inputing in the text box", function(){
-        cy.get(".extend-region").within(() => {
-            cy.get("input.form-control").eq(1).clear().type("138k{enter}",{delay:100});
-            cy.get("input.form-control").eq(1).should("have.value","138k");
+        cy.get(".input-section").within(() => {
+            cy.get("textarea").clear().type("MAL1:40000..60000",{delay:100});
+            cy.get(".extend-region").within(() => {
+                cy.get("input.form-control").eq(1).clear().type("38k{enter}",{delay:100});
+                cy.get("input.form-control").eq(1).should("have.value","38k");
+            })
+            cy.get("button").filter(':contains("Search")').click();
+        })
+        cy.get(".results").within(() => {
+            cy.get("#region-result-0").should("include.text","MAL1 2000..98000");
         })
     })
 
     it("can lock gene search region extension", function(){
-        cy.get(".extend-region").within(() => {
-            cy.get(".icon-lock").click();
-            cy.get(".slider-ticks").eq(0).within(() => {
-                cy.contains("100k").click();
-                cy.wait(500);
+        cy.get(".input-section").within(() => {
+            cy.get("textarea").clear().type("MAL1:10000..20000",{delay:100});
+            cy.get(".extend-region").within(() => {
+                cy.get(".icon-lock").click();
+                cy.get("input.form-control").eq(1).clear().type("3.8k{enter}",{delay:100});
+                cy.get("input.form-control").eq(2).clear().type("2500{enter}",{delay:100});
+                cy.get("input.form-control").eq(1).should("have.value","3.8k");
+                cy.get("input.form-control").eq(2).should("have.value","2500");
             })
-            cy.get("input.form-control").eq(1).should("have.value","100k");
-            cy.get("input.form-control").eq(2).should("have.value","0");
+            cy.get("button").filter(':contains("Search")').click();
         })
+        cy.get(".results").within(() => {
+            cy.get("#region-result-0").should("include.text","MAL1 6200..22500");
+        })
+
     })
 
     it("can select the organism in which to perform region search", function(){
-        cy.get(".organism-selection").within(() => {
-            cy.get(".dropdown-toggle").click();
-            cy.get(".dropdown-menu").within(() => {
-                cy.contains("Clear").click();
+        cy.get(".input-section").within(() => {
+            cy.get("textarea").clear().type("MAL1:10000..40000",{delay:100});
+            cy.get(".organism-selection").within(() => {
+                cy.get(".dropdown-toggle").click();
+                cy.get(".dropdown-menu").within(() => {
+                    cy.contains("Clear").click();
+                })
+                cy.get("span").should("include.text","All Organisms");
             })
-            cy.get("span").should("include.text","All Organisms");
+            cy.get("button").filter(':contains("Search")').click();
+        })
+        cy.get(".results").within(() => {
+            cy.get(".features-count").should("include.text","overlapping features");
         })
     })
 
