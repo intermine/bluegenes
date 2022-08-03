61e00a0f857cba2ad633930c3d41c5484c879f96
heralden <regen@anche.no>
2022-08-03 19:38:10 +0100

    CI: Fix flaky User Account Test can delete an account

--- cypress/integration/user_account/user_account_test.js
+++ cypress/integration/user_account/user_account_test.js
@@ -144,8 +144,17 @@ describe("User Account Test", function() {
             })
             cy.get("button").contains("Delete account").click();
         })
-        cy.intercept("POST","/api/auth/logout").as("logout");
-        cy.wait("@logout");
-        cy.get(".mine-intro").should("exist");
+        cy.openLoginDialogue();
+        cy.get(".login-form").should("contain", "Login to BioTestMine");
+        cy.get("input#email").type("test_user_account@mail.com");
+        cy.get("input[type='password']").type("newpassword");
+        cy.intercept('POST', '/api/auth/login').as('login');
+        cy.get(".login-form")
+          .find("button")
+          .contains('Login')
+          .click();
+        cy.wait('@login');
+        cy.get(".error-box")
+          .should('to.contain', 'Unknown username: test_user_account@mail.com');
     })
 });
